8821a6dff15acdabf3c5bfbe75ec53188ec417bb
yuushiba <shibata.yuichi79@gmail.com>
2025-01-13 17:42:14 +0900

    add:DaisyUI実装完了

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './app/components/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"], // 使用するテーマを指定
+  },
 }
