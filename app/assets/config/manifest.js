4876dbd4dd23b9c7b53b438174eef1fb66da6e4c
yuushiba <shibata.yuichi79@gmail.com>
2025-01-18 23:05:01 +0900

    add:

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
+// app/assets/config/manifest.js
 //= link_tree ../images
-//= link_tree ../builds
-//= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_tree ../builds
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,25 @@
-// tailwind.config.js
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js',
-    './app/components/**/*.{erb,haml,html,slim}'
+    './app/javascript/**/*.js'
   ],
   theme: {
     extend: {
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
       },
-      colors: {
-        'sakura-bg': '#E8EBF2',
-        'sakura-accent': '#B9AAD3',
-      }
     },
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        sakura: {
-          "primary": "#B9AAD3",
-          "secondary": "#E8EBF2",
-          "accent": "#B9AAD3",
-          "neutral": "#B9AAD3",
-          "base-100": "#E8EBF2",
-          "base-200": "#B9AAD3",
-          "base-300": "#B9AAD3",
-          "base-content": "#B9AAD3",
-          "neutral-content": "#FFFFFF",
-          "--rounded-box": "1rem",
-          "--rounded-btn": "0.5rem",
-          "--rounded-badge": "1.9rem",
-        },
-      },
-    ],
-  },
+    themes: [{
+      sakura: {
+        "primary": "#DD7594",
+        "base-100": "#E8EBF2",
+        "primary-content": "#FFFFFF",
+      }
+    }]
+  }
 }
