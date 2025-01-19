b3046cccd5ff8f7d02e7ab48bfeac9568b2f6952
yuushiba <shibata.yuichi79@gmail.com>
2025-01-19 20:49:29 +0900

    add:モデルとルートとusers_controllerにコード記載

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  resources :users, only: %i[new create]
   # Defines the root path route ("/")
   # root "posts#index"
 end
