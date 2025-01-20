a60d4d2b766b725f9d9e30d4be84f45536c3a880
yuushiba <shibata.yuichi79@gmail.com>
2025-01-20 20:40:16 +0900

    add:ログインとログアウト機能実装動作確認済

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,21 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to root_path, success: "ログインしました"
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, status: :see_other, success: "ログアウトしました"
+  end
 end
