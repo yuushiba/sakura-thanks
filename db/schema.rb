e6797731c3e55b54c09e751c98f418abe24d623b
yuushiba <shibata.yuichi79@gmail.com>
2025-02-09 21:46:33 +0900

    add:postsモデルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_04_111144) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_09_124007) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_04_111144) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "overlay_text"
+    t.integer "text_x_position", default: 0
+    t.integer "text_y_position", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
