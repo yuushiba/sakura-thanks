aa22c02964da98e2c68a0f9dbdd23ba632e6ef72
yuushiba <shibata.yuichi79@gmail.com>
2025-03-15 19:33:01 +0900

    add:プライバシーポリシー作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[top form terms privacy]
   def top; end
   def form; end
+  def terms; end  # 利用規約表示用
+  def privacy; end  # プライバシーポリシー表示用
 end
