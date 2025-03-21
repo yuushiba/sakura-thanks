8ace66871fda093f5e9d2a4c2a177b98f373a233
yuushiba <shibata.yuichi79@gmail.com>
2025-03-21 22:16:48 +0900

    add:テストコードと環境変数の不一致解消

--- spec/support/selenium_helper.rb
+++ spec/support/selenium_helper.rb
@@ -3,29 +3,28 @@ require 'capybara/rspec'
 # CI環境かどうかを判定
 is_ci = ENV['CI'].present?
 
+# ドライバーの登録
+Capybara.register_driver :selenium_remote_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+
+  # 環境変数からURLを取得（環境変数がなければデフォルト値を使用）
+  remote_url = ENV['SELENIUM_REMOTE_URL'] || 'http://selenium_chrome:4444/wd/hub'
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :remote,
+    url: remote_url,
+    capabilities: options
+  )
+end
+
 # システムテスト設定
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    # 環境変数から接続先を取得するか、デフォルト値を使用
-    remote_url = ENV['SELENIUM_REMOTE_URL'] || 'http://selenium_chrome:4444/wd/hub'
-
-    # Seleniumドライバー設定
     driven_by :selenium_remote_chrome, screen_size: [ 1400, 1400 ]
-
-    # ドライバーの登録（前に追加）
-    Capybara.register_driver :selenium_remote_chrome do |app|
-      options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--headless')
-      options.add_argument('--no-sandbox')
-      options.add_argument('--disable-dev-shm-usage')
-
-      Capybara::Selenium::Driver.new(
-        app,
-        browser: :remote,
-        url: remote_url,
-        capabilities: options
-      )
-    end
 
     # サーバー設定
     Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
