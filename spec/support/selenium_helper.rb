596237f559fa0c404d707ec3cc5c4ea68995e55c
yuushiba <shibata.yuichi79@gmail.com>
2025-03-22 20:36:22 +0900

    add:明示的に設定

--- spec/support/selenium_helper.rb
+++ spec/support/selenium_helper.rb
@@ -15,10 +15,17 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     # CI環境とローカル環境で異なる設定
     if ENV['CI']
-      # CI環境ではChromeのヘッドレスモードを直接使用
-      # カスタム設定を追加してヘッドレスにする
-      driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
-      puts "🌐 CI環境用のブラウザ設定を使用します (標準ヘッドレスChrome)"
+      driven_by :selenium, using: :chrome, options: {
+        browser: :chrome,
+        capabilities: [
+          Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+            opts.add_argument('--headless')
+            opts.add_argument('--no-sandbox')
+            opts.add_argument('--disable-dev-shm-usage')
+          end
+        ]
+      }
+      puts "🌐 CI環境用のローカルChromeブラウザ設定を使用します"
     else
       # ローカル環境では以前と同じ設定
       remote_url = 'http://selenium_chrome:4444/wd/hub'
