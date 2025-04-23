c5b53629ebc3da85610650d3770124a691e2efbb
yuushiba <shibata.yuichi79@gmail.com>
2025-04-23 20:16:40 +0900

    add:ブックマークを非同期。白黒でフラッシュメッセージ表示されず

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,20 +3,34 @@ class BookmarksController < ApplicationController
     @post = Post.find(params[:post_id])
     current_user.bookmark(@post)
 
-    # 通知メッセージだけを追加
-    flash[:success] = "投稿をブックマークしました"
-
-    # リダイレクト
-    redirect_back(fallback_location: posts_path)
+    respond_to do |format|
+      format.html { 
+        flash[:success] = "投稿をブックマークしました"
+        redirect_back(fallback_location: posts_path) 
+      }
+      format.turbo_stream { 
+        flash.now[:success] = "投稿をブックマークしました"
+        render turbo_stream: turbo_stream.replace("bookmark_btn_#{@post.id}", 
+          partial: 'application/bookmark_button', locals: { post: @post }) 
+      }
+    end
   end
 
   def destroy
     @bookmark = current_user.bookmarks.find(params[:id])
-    post = @bookmark.post
-    current_user.unbookmark(post)
+    @post = @bookmark.post
+    current_user.unbookmark(@post)
 
-    flash[:success] = "ブックマークを削除しました"
-    # status: :see_other を外し、redirect_backにオプションとして渡す
-    redirect_back(fallback_location: posts_path, status: :see_other)
+    respond_to do |format|
+      format.html { 
+        flash[:success] = "ブックマークを削除しました"
+        redirect_back(fallback_location: posts_path, status: :see_other) 
+      }
+      format.turbo_stream { 
+        flash.now[:success] = "ブックマークを削除しました"
+        render turbo_stream: turbo_stream.replace("bookmark_btn_#{@post.id}", 
+          partial: 'application/bookmark_button', locals: { post: @post }) 
+      }
+    end
   end
 end
